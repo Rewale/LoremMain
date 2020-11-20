9c98df9cde7528ed70ecf022e85e59ea94d7e168
Rewale <44743547+Rewale@users.noreply.github.com>
2020-11-20 11:56:18 +0500

    Add files via upload

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,17 +1,46 @@
 
 $( document ).ready(function(){
-$('.list-hide').hide();
-$('#hide-div').hide();
-if($(window).width()<=680)
-{
-    $('.list').hide();
-    $('#Buttons-nav').hide();
-    $('#hide-div').show();
+    /*При загрузке страницы прячем иконку 
+    меню и выплывающее меню*/
+    $('.list-hide').hide();
+    $('#hide-div').hide();
+    /*Так же привязываем иконке
+    меню событие*/
     $('#menu_butt').click(function(){
         $('.list-hide').slideToggle(500)
     })
-}
-})
+    // Проверка размера экрана при ЗАГРУЗКЕ страницы
+    if($(window).width()<=680)
+    {
+        // Прячем навигацию с кнопкам
+        // Показываем меню и иконку
+        $('.list').hide();
+        $('#Buttons-nav').hide();
+        $('#hide-div').show();
+        
+    }    
+    //Проверка размера экрана при ИЗМЕНЕНИИ РАЗМЕРА экрана
+    $( window ).resize(function() {
+        if($(window).width()<=680)
+        {
+            // Прячем навигацию с кнопкам
+            // Показываем меню и иконку
+            $('.list').hide();
+            $('#Buttons-nav').hide();
+            $('#hide-div').show();            
+        }
+        else{
+            // Показываем навигацию с кнопкам
+            // Прячем меню и иконку
+            $('.list').show();
+            $('#Buttons-nav').show();
+            $('#hide-div').hide();
+                      
+        }
+    })
+});
+
+
 
 
 
